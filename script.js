38565418095123395ee809d938f18ba5c1f74aac
mohammed kaif <mkaif555123@gmail.com>
2024-04-26 21:50:04 +0500

    100% WORK DONE

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let user = document.getElementById("date");
 user.max = new Date().toISOString().split("T")[0];
+let answer = document.getElementById("answer");
+let tDate = document.getElementById("t-date");
+tDate.innerHTML = new Date().toDateString();
 
 function ageCheck() {
   let ageDate = new Date(user.value);
@@ -14,5 +17,31 @@ function ageCheck() {
   let month2 = today.getMonth() + 1;
   let year2 = today.getFullYear();
 
-  
+  let date3, month3, year3;
+
+  year3 = year2 - year1;
+  if (month2 >= month1) {
+    month3 = month2 - month1;
+  } else {
+    year3--;
+    month3 = 12 + month2 - month1;
+  }
+
+  if (date2 >= date1) {
+    date3 = date2 - date1;
+  } else {
+    month3--;
+    date3 = getDaysMonth(year1, month1) + date2 - date1;
+  }
+
+  if (month3 < 0) {
+    month3 = 11;
+    year3--;
+  }
+
+  answer.innerHTML = `YOU ARE ${year3} YEARS , ${month3} MONTHS AND ${date3} DAYS OLD`;
+}
+
+function getDaysMonth(year, month) {
+  return new Date(year, month, 0).getDate();
 }
